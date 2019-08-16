59bff6afee6c5193f20d4ef79f99b147c24b90a7
Atsushi Sakai <asakai.amsl+github@gmail.com>
2019-08-16 23:39:07 +0900

    keep implement

--- pyrooombaadapter/pyroombaadapter.py
+++ pyrooombaadapter/pyroombaadapter.py
@@ -2,10 +2,11 @@
 A Python module for Roomba Open Interface
 """
 import math
+from enum import Enum
 from time import sleep
 
 import byte_tool
-import serial
+import serial  # pyserial
 
 
 class PyRoombaAdapter:
@@ -56,11 +57,16 @@ class PyRoombaAdapter:
         "MAX_VELOCITY": 500,
     }
 
+    class Direction(Enum):
+        clockwise = 1
+        counter_clockwise = 2
+
     def __init__(self, port, bau_rate=115200, time_out_sec=1., wheel_span_mm=235.0):
         self.WHEEL_SPAN = wheel_span_mm
         self.serial_con = self._connect_serial(port, bau_rate, time_out_sec)
 
         self.change_mode_to_safe()  # default mode is safe mode
+        sleep(1.0)
 
     def __del__(self):
         """
@@ -217,8 +223,7 @@ class PyRoombaAdapter:
 
         - Available in modes: Safe or Full
         - Changes mode to: No Change
-        - Velocity (-500 – 500 mm/s)
-        - Radius (-2000 – 2000 mm)
+
         - Special cases:
             - Straight = 32768 or 32767 = hex 8000 or 7FFF
             - Turn in place clockwise = -1
@@ -237,14 +242,12 @@ class PyRoombaAdapter:
             >>> time.sleep(1.0) # keep rotate
             >>> adapter.move(0.1, 0.0) # move straight with 10cm/sec
         """
-        if yaw_rate >= 0:
-            direction = 'CCW'
-        else:
-            direction = 'CW'
-
         if velocity == 0:  # rotation
             vel_mm_sec = math.fabs(yaw_rate) * (self.WHEEL_SPAN / 2.0)
-            radius_mm = 0
+            if yaw_rate >= 0:
+                radius_mm = 1
+            else:  # default is 'CCW' (turning left)
+                radius_mm = -1
         elif yaw_rate == 0:
             vel_mm_sec = velocity * 1000.0  # m/s -> mm/s
             radius_mm = self.PARAMS["STRAIGHT_RADIUS"]
@@ -252,34 +255,144 @@ class PyRoombaAdapter:
             vel_mm_sec = velocity * 1000.0  # m/s -> mm/s
             radius_mm = vel_mm_sec / yaw_rate
 
-        self.send_drive_cmd(vel_mm_sec, radius_mm, direction)
+        self.send_drive_cmd(vel_mm_sec, radius_mm)
 
-    def send_drive_cmd(self, roomba_mm_sec, roomba_radius_mm, turn_dir='CCW'):
+    def send_drive_cmd(self, roomba_mm_sec, roomba_radius_mm):
         """
         send drive command
 
         This command controls Roomba’s drive wheels.
         The radius is measured from the center of the turning circle to the center of Roomba.
-        A Drive command with a positive velocity and a positive radius makes Roomba drive forward while turning toward the left.
+        A Drive command with a positive velocity and a positive radius makes Roomba drive forward while turning left.
         A negative radius makes Roomba turn toward the right.
         Special cases for the radius make Roomba turn in place or drive straight.
         A negative velocity makes Roomba drive backward.
 
         Args:
             - roomba_mm_sec: the average velocity of the drive wheels in millimeters per second (mm/s)
+              (-500 – 500 mm/s)
             - roomba_radius_mm: the radius in millimeters at which Roomba will turn.
+              (-2000 – 2000 mm)
             - turn_dir: turning direction, it should be CW:clock wise or CCW: counter clock wise
+        Examples:
+            >>> PORT = "/dev/ttyUSB0"
+            >>> adapter = PyRoombaAdapter(PORT)
+            >>> adapter.send_drive_cmd(-100, -1000) # back to the right side
+            >>> sleep(2.0) # keep 2 sec
         """
         # print(roomba_mm_sec, roomba_radius_mm, turn_dir)
 
-        roomba_mm_sec = self._adjust_velocity(roomba_mm_sec)
+        roomba_mm_sec = self._adjust_min_max(roomba_mm_sec, self.PARAMS["MIN_VELOCITY"], self.PARAMS["MAX_VELOCITY"])
         velHighVal, velLowVal = byte_tool.get_2_bytes(roomba_mm_sec)
 
-        roomba_radius_mm = self._adjust_radius(roomba_radius_mm, turn_dir)
+        roomba_radius_mm = self._adjust_min_max(roomba_radius_mm, self.PARAMS["MIN_RADIUS"], self.PARAMS["MAX_RADIUS"])
         radiusHighVal, radiusLowVal = byte_tool.get_2_bytes(roomba_radius_mm)
 
         # send these bytes and set the stored velocities
         self._send_cmd([self.CMD["Drive"], velHighVal, velLowVal, radiusHighVal, radiusLowVal])
+
+    def send_drive_direct(self, right_mm_sec, left_mm_sec):
+        """
+        send drive direct command
+
+        This command lets you control the forward and backward motion of Roomba’s drive wheels independently.
+        A positive velocity makes that wheel drive forward, while a negative velocity makes it drive backward.
+
+        - Available in modes: Safe or Full
+
+        Args:
+            - right_mm_sec: the average velocity of the right drive wheels in millimeters per second (mm/s)
+              (-500 – 500 mm/s)
+            - left_mm_sec: the average velocity of the left drive wheels in millimeters per second (mm/s)
+              (-500 – 500 mm/s)
+        Examples:
+            >>> PORT = "/dev/ttyUSB0"
+            >>> adapter = PyRoombaAdapter(PORT)
+            >>>> adapter.send_drive_direct(10, 100) # move right forward
+            >>> sleep(2.0) # keep 2 sec
+        """
+        right_mm_sec = self._adjust_min_max(right_mm_sec, self.PARAMS["MIN_VELOCITY"], self.PARAMS["MAX_VELOCITY"])
+        right_high, right_low = byte_tool.get_2_bytes(right_mm_sec)
+
+        left_mm_sec = self._adjust_min_max(left_mm_sec, self.PARAMS["MIN_VELOCITY"], self.PARAMS["MAX_VELOCITY"])
+        left_high, left_low = byte_tool.get_2_bytes(left_mm_sec)
+
+        # send these bytes and set the stored velocities
+        self._send_cmd([self.CMD["Drive Direct"], right_high, right_low, left_high, left_low])
+
+    def send_drive_pwm(self, right_pwm, left_pwm):
+        """
+        send drive pwm command
+
+        This command lets you control the forward and backward motion of Roomba’s drive wheels independently.
+        It takes four data bytes, which are interpreted as two 16-bit signed values using two’s complement.
+        A positive PWM makes that wheel drive forward, while a negative PWM makes it drive backward.
+
+        - Available in modes: Safe or Full
+
+        Args:
+            - right_pwm: Right wheel PWM (-255 – 255)
+            - left_pwm: Left wheel PWM (-255 - 255)
+
+        Examples:
+            >>> PORT = "/dev/ttyUSB0"
+            >>> adapter = PyRoombaAdapter(PORT)
+            >>> adapter.send_drive_pwm(-200, -200) # move backward
+            >>> sleep(2.0) # keep 2 sec
+        """
+        right_pwm = self._adjust_min_max(right_pwm, -255, 255)
+        right_high, right_low = byte_tool.get_2_bytes(right_pwm)
+
+        left_pwm = self._adjust_min_max(left_pwm, -255, 255)
+        left_high, left_low = byte_tool.get_2_bytes(left_pwm)
+
+        # send these bytes and set the stored velocities
+        self._send_cmd([self.CMD["Drive PWM"], right_high, right_low, left_high, left_low])
+
+    def send_moters_cmd(self, main_brush_on, main_brush_direction_is_ccw,
+                        side_brush_on, side_brush_direction_is_inward,
+                        vacuum_on
+                        ):
+        """
+        send moters command
+
+        This command controls the motion of Roomba’s main brush, side brush, and vacuum independently.
+        Motor velocity cannot be controlled with this command, all motors will run at maximum speed when enabled.
+        The main brush and side brush can be run in either direction. The vacuum only runs forward.
+
+        :param bool main_brush_on: main brush on or off
+
+        :param bool main_brush_direction_is_ccw: main brush direction, clockwise or counter-clockwise(default)
+
+        :param bool side_brush_on: side brush on or off
+
+        :param bool side_brush_direction_is_inward: side brush direction, inward(default) or outward
+
+        :param bool side_brush_on: side brush on or off
+
+        :param bool vacuum_on: vacuum on or off
+
+        Examples:
+            >>> PORT = "/dev/ttyUSB0"
+            >>> adapter = PyRoombaAdapter(PORT)
+            >>> adapter.send_moters_cmd(False, True, True, True, False) # side brush is on, and it rotates inward
+            >>> sleep(2.0) # keep 2 sec
+        """
+        cmd = 0  # All initial bit is 0
+        if side_brush_on:
+            cmd |= 0b00000001
+        if vacuum_on:
+            cmd |= 0b00000010
+        if main_brush_on:
+            cmd |= 0b00000100
+        if not side_brush_direction_is_inward:
+            cmd |= 0b00001000
+        if not main_brush_direction_is_ccw:
+            cmd |= 0b00010000
+
+        print(cmd)
+        print(bin(cmd))
+        self._send_cmd([self.CMD["Moters"], cmd])
 
     @staticmethod
     def _connect_serial(port, bau_rate, time_out):
@@ -290,34 +403,19 @@ class PyRoombaAdapter:
             print('Serial port did NOT open')
         return serial_con
 
-    def _adjust_radius(self, radius, turn_dir):
+    @staticmethod
+    def _adjust_min_max(val, min_val, max_val):
 
-        if type(radius) != int:
-            radius = int(radius)
+        # integer cast
+        if type(val) != int:
+            val = int(val)
 
-        if radius < self.PARAMS["MIN_RADIUS"]:
-            radius = self.PARAMS["STRAIGHT_RADIUS"]
-        if radius > self.PARAMS["MAX_RADIUS"]:
-            radius = self.PARAMS["STRAIGHT_RADIUS"]
+        if val < min_val:
+            val = min_val
+        elif val > max_val:
+            val = max_val
 
-        if radius == 0:
-            if turn_dir == 'CW':
-                radius = -1
-            else:  # default is 'CCW' (turning left)
-                radius = 1
-        return radius
-
-    def _adjust_velocity(self, velocity):
-
-        if type(velocity) != int:
-            velocity = int(velocity)
-
-        if velocity < self.PARAMS["MIN_VELOCITY"]:
-            velocity = self.PARAMS["MIN_VELOCITY"]
-        elif velocity > self.PARAMS["MAX_VELOCITY"]:
-            velocity = self.PARAMS["MAX_VELOCITY"]
-
-        return velocity
+        return val
 
     def _send_cmd(self, cmd):
         if type(cmd) == list:  # command list
@@ -329,8 +427,15 @@ class PyRoombaAdapter:
 def main():
     PORT = "/dev/ttyUSB0"
     adapter = PyRoombaAdapter(PORT)
-    adapter.turn_off_power()
-    # sleep(1.0)
+    # adapter.send_drive_cmd(-100, -1000)
+    # adapter.send_drive_direct(-100, 100)
+    # import numpy as np
+    # adapter.move(0.1, np.deg2rad(-10))
+    # adapter.send_drive_pwm(80, 80)
+    # adapter.send_drive_pwm(-200, -200)
+    adapter.send_moters_cmd(False, True, True, True, False)
+    sleep(1.0)
+
     # adapter.move(0, np.deg2rad(-10))
     # sleep(1.0)
     # adapter.move(0, np.deg2rad(10))
