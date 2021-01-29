9d21d2ecaedad9bafade39e67ece71c0f2c48f04
Atsushi Sakai <asakai.amsl+github@gmail.com>
2021-01-29 21:38:26 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 """
 Setup script
 How to upload new release
-1. change version in setup.py
+1. bump version with bump_version.sh
 2. setup twine, see:https://blog.amedama.jp/entry/2017/12/31/175036
 3. create zip file: python setup.py sdist
-4. upload twine upload --repository pypi dist/hogehoeg
+4. check upload possible: twine check dist/pyroombaadapter-0.1.2.tar.gz
+5 upload: twine upload --repository pypi dist/pyroombaadapter-0.1.2.tar.gz
 """
 import os
 
@@ -15,13 +16,18 @@ PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 # read README
 try:
     import pypandoc
+
     readme = pypandoc.convert_file(PROJECT_PATH + '/README.md', 'rst')
 except(IOError, ImportError):
     readme = open(PROJECT_PATH + '/README.md').read()
 
+# read VERSION
+with open(PROJECT_PATH + "/VERSION", 'r') as fd:
+    VERSION = fd.readline().rstrip('\n')
+
 setup(
     name="pyroombaadapter",
-    version="0.1.2",
+    version=VERSION,
     url="https://github.com/AtsushiSakai/PyRoombaAdapter",
     author="Atsushi Sakai",
     author_email="asakaig@gmail.com",
