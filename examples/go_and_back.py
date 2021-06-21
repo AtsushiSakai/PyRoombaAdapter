c06d2225b24291546535e818ec946d0b9d9d7b34
mrkeuz <mrkeuz@users.noreply.github.com>
2021-06-22 02:58:07 +0300

    Issue #18 Replace np.deg2rad(x) to math.radians(x) in examples and docs
    Replace numpy function to python standard library calls for simplicity and avoid extra dependencies

--- examples/go_and_back.py
+++ examples/go_and_back.py
@@ -3,16 +3,16 @@
 """
 from time import sleep
 
-import numpy as np
+import math
 from pyroombaadapter import PyRoombaAdapter
 
 PORT = "/dev/ttyUSB0"
 adapter = PyRoombaAdapter(PORT)
-adapter.move(0.2, np.deg2rad(0.0))  # go straight
+adapter.move(0.2, math.radians(0.0))  # go straight
 sleep(1.0)
-adapter.move(0, np.deg2rad(-20))  # turn right
+adapter.move(0, math.radians(-20))  # turn right
 sleep(6.0)
-adapter.move(0.2, np.deg2rad(0.0))  # go straight
+adapter.move(0.2, math.radians(0.0))  # go straight
 sleep(1.0)
-adapter.move(0, np.deg2rad(20))  # turn left
+adapter.move(0, math.radians(20))  # turn left
 sleep(6.0)
