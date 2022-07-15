0dab9097820fcd8d2c0b6b9550e1559b34e7859e
Atsushi Sakai <asakai.amsl+github@gmail.com>
2022-07-15 21:35:14 +0900

    update doc system

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ author = 'Atsushi Sakai'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
